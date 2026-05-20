fd0184d539d6d25335c25ca21c517dbd1a419f9d
Evanio Tech <evanioshark@gmail.com>
2026-05-20 14:32:19 -0300

    fix: ajuste na sction inicio

--- app/page.tsx
+++ app/page.tsx
@@ -5,15 +5,15 @@ export default function Home() {
     <main className="min-h-screen bg-gradient-to-r from-purple-500 to-pink-500">
       <Header />
 
-      <section id="inicio" className="mt-4">
-        <p className="text-white text-center text-sm sm:text-base text-lg lg:px-40">
+      <section id="inicio" className="mt-4 pt-25 sm:p-18 px-4">
+        <p className="text-white text-center text-sm sm:text-base sm:text9xl text-lg lg:px-40">
           Seja Bem vindo ao Charme Chicc, onde a elegância encontra a sofisticação. Descubra uma coleção exclusiva de roupas e acessórios que combinam estilo e conforto para realçar sua beleza única. Explore nossas últimas tendências e encontre peças que refletem sua personalidade. Charme Chicc - onde cada detalhe é pensado para você brilhar.
         </p>
       </section>
       <section id="produtos">
         <h2 className="text-white text-center text-2xl mt-8">Produtos</h2>
         {/* Adicione aqui a seção de produtos */}
-        <p className="text-white text-center  text-sm sm:text-base text-lg lg:px-40">
+        <p className="text-white text-center  text-sm sm:text-base text-lg lg:px-30">
           
         </p>
       </section>
