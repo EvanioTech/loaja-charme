bf8eb7b42868c74cd98437ef03c0d8ad71912309
Evanio Tech <evanioshark@gmail.com>
2026-05-20 14:02:58 -0300

    feat: criado o componente header

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,9 @@
+import { Header } from "@/components/Header";
+
 export default function Home() {
   return (
-    <main className="min-h-screen bg-gradient-to-r from-purple-500 to-pink-500 flex flex-col justify-center items-center px-4">
-      <header>
-        <h1 className="text-white text-4xl sm:text-6xl md:text-8xl font-bold text-center">
-          Charme Chicc
-        </h1>
-      </header>
+    <main className="min-h-screen bg-gradient-to-r from-purple-500 to-pink-500">
+      <Header />
 
       <section className="mt-4">
         <p className="text-white text-center text-sm sm:text-base text-lg lg:px-40">
---
+++ components/Header.tsx
@@ -0,0 +1,22 @@
+
+
+
+export function Header() {
+  return (
+    <header className="w-full flex justify-between items-center p-4">
+      <h1 className="text-white text-2xl font-bold">
+        Charme Chicc
+      </h1>
+
+      <nav>
+        <ul className="flex gap-4 text-white">
+          <li>Home</li>
+          <li>Produtos</li>
+          <li>Contato</li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+
