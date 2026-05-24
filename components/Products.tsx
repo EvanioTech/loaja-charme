60bf339c736463721070cdaa298a239b13396609
Evanio Tech <evanioshark@gmail.com>
2026-05-24 10:13:56 -0300

    feat: adicionando titulo e preço ao produto

--- components/Products.tsx
+++ components/Products.tsx
@@ -7,7 +7,7 @@ export function Products() {
       
 
       <div className="grid grid-cols-1 md:grid-cols-3 gap-6 px-4">
-        <article className="bg-white rounded-2xl p-4 h-60">
+        <article className="bg-white rounded-2xl p-4 h-100 relative">
           <Image
             src="/produtos/i1.webp"
             alt="Produto Charme Chicc 1"
@@ -15,9 +15,11 @@ export function Products() {
             height={400}
             className="object-cover rounded-2xl w-full h-full"
           />
+          <p className="absolute bottom-6 left-6 bg-black/50 text-white font-bold px-2 py-1 rounded-lg ">Vestido</p>
+          <p className="absolute bottom-6 right-6 bg-black/50 text-white px-2 py-1 rounded-lg font-bold">45,00</p>
         </article>
 
-        <article className="bg-white rounded-2xl p-4 h-60">
+        <article className="bg-white rounded-2xl p-4 h-100 relative">
           <Image
             src="/produtos/i2.webp"
             alt="Produto Charme Chicc 2"
@@ -25,9 +27,11 @@ export function Products() {
             height={400}
             className="object-cover rounded-2xl w-full h-full"
           />
+          <p className="absolute bottom-6 left-6 bg-black/50 text-white font-bold px-2 py-1 rounded-lg ">Vestido</p>
+          <p className="absolute bottom-6 right-6 bg-black/50 text-white px-2 py-1 rounded-lg font-bold">45,00</p>
         </article>
 
-        <article className="bg-white rounded-2xl p-4 h-60">
+        <article className="bg-white rounded-2xl p-4 h-100 relative">
           <Image
             src="/produtos/i3.webp"
             alt="Produto Charme Chicc 3"
@@ -35,8 +39,10 @@ export function Products() {
             height={400}
             className="object-cover rounded-2xl w-full h-full"
           />
+          <p className="absolute bottom-6 left-6 bg-black/50 text-white font-bold px-2 py-1 rounded-lg ">Vestido</p>
+          <p className="absolute bottom-6 right-6 bg-black/50 text-white px-2 py-1 rounded-lg font-bold">45,00</p>
         </article>
-        <article className="bg-white rounded-2xl p-4 h-60">
+        <article className="bg-white rounded-2xl p-4 h-100 relative">
           <Image
             src="/produtos/i4.webp"
             alt="Produto Charme Chicc 4"
@@ -44,8 +50,10 @@ export function Products() {
             height={400}
             className="object-cover rounded-2xl w-full h-full"
           />
+          <p className="absolute bottom-6 left-6 bg-black/50 text-white font-bold px-2 py-1 rounded-lg ">Vestido</p>
+          <p className="absolute bottom-6 right-6 bg-black/50 text-white px-2 py-1 rounded-lg font-bold">45,00</p>  
         </article>
-        <article className="bg-white rounded-2xl p-4 h-60">
+        <article className="bg-white rounded-2xl p-4 h-100 relative">
           <Image
             src="/produtos/i5.webp"
             alt="Produto Charme Chicc 5"
@@ -53,9 +61,11 @@ export function Products() {
             height={400}
             className="object-cover rounded-2xl w-full h-full"
           />
+          <p className="absolute bottom-6 left-6 bg-black/50 text-white font-bold px-2 py-1 rounded-lg ">Vestido</p>
+          <p className="absolute bottom-6 right-6 bg-black/50 text-white px-2 py-1 rounded-lg font-bold">45,00</p>
         </article>
         
-        <article className="bg-white rounded-2xl p-4 h-60">
+        <article className="bg-white rounded-2xl p-4 h-100 relative">
           <Image
             src="/produtos/i6.webp"
             alt="Produto Charme Chicc 6"
@@ -63,6 +73,9 @@ export function Products() {
             height={400}
             className="object-cover rounded-2xl w-full h-full"
           />
+          <p className="absolute bottom-6 left-6 bg-black/50 text-white font-bold px-2 py-1 rounded-lg ">Vestido</p>
+          <p className="absolute bottom-6 right-6 bg-black/50 text-white px-2 py-1 rounded-lg font-bold">45,00</p>
+        
         </article>
 
       </div>
