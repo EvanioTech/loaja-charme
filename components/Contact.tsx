f7c68da297b673ad5f425d5df59b92e8a1af7b9d
Evanio Tech <evanioshark@gmail.com>
2026-05-24 17:56:36 -0300

    fix: ajuste link instagram

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -6,7 +6,7 @@ export function Contact() {
     <div className="flex-row gap-4 mt-10 justify-center items-center flex pb-8">
       <span >
         
-        <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer" className="text-white flex-row flex gap-4 items-center">
+        <a href="https://www.instagram.com/charme_chicc/" target="_blank" rel="noopener noreferrer" className="text-white flex-row flex gap-4 items-center">
           <Image src={iconi} alt="Instagram" width={32} height={32} className='rounded-full' /> @charmechicc
         </a>
       </span>
